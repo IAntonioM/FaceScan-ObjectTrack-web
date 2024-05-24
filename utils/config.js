473375761e2c6c7aa523a6903b4c17de5e81c1e0
Antonio M <antoniomayhuay@gmail.com>
2024-05-24 11:57:56 -0500

    api_24_05

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://b974-2001-1388-1e42-e86-bc32-2e27-c8ac-5258.ngrok-free.app';
+const API_URL = 'https://34a2-2001-1388-1e42-e86-f42b-21fa-51f-b10f.ngrok-free.app';
 
 
 // ngrok http http://localhost:5000 --host-header="localhost:5000"

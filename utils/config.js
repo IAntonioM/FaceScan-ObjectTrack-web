260eee72879922934b4ba7b25378a3c8df7ee572
Antonio M <antoniomayhuay@gmail.com>
2024-07-04 00:49:02 -0500

    vfsdsd

--- utils/config.js
+++ utils/config.js
@@ -2,5 +2,13 @@ const API_URL = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.a
 
 
 
+const API_URL2 = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.app';
+
+const API_URL3 = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.app';
+
+
+
+
+
 
 // ngrok http http://localhost:5000 --host-header="localhost:5000"

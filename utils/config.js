b0b31cc88790639e1297e1d177c06fe5831c1abb
Antonio M <antoniomayhuay@gmail.com>
2024-07-04 06:56:12 -0500

    api_04_07

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,4 @@
-const API_URL = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.app';
-
-
-
-const API_URL2 = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.app';
-
-const API_URL3 = 'https://d2ba-2001-1388-1e42-29f2-ddd-a052-8847-80c.ngrok-free.app';
+const API_URL = 'https://90ba-181-65-101-145.ngrok-free.app';
 
 
 
